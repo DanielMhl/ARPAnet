6333d3cced654be745608d7e201514bf2870a458
Diego.Fast <dieggo.albuquerque@gmail.com>
2022-11-24 12:20:20 -0300

    #migrations - add nomeProduto

--- public/utilitarios [executar primeiro]/arpa.sql
+++ public/utilitarios [executar primeiro]/arpa.sql
@@ -3,3 +3,22 @@ CREATE SCHEMA arpanet DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 INSERT INTO `arpanet`.`users` (`name`, `email`, `password`, `tipo`, `created_at`, `updated_at`)
 VALUES ('root', 'root@outlook.com', '$2y$10$IFRVH0.rUGzyKFPTX/z7kuSxbm4bq1eSQXh3GS20CEO7DN09oUVDm', 'admin', '2022-11-23 21:34:44', '2022-11-23 21:34:44');
 
+INSERT INTO `arpanet`.`users` (`name`, `email`, `password`, `tipo`, `created_at`, `updated_at`)
+VALUES
+('Pedro Guimarães', 'pedro@outlook.com', '$2y$10$IFRVH0.rUGzyKFPTX/z7kuSxbm4bq1eSQXh3GS20CEO7DN09oUVDm', 'admin', '2022-11-23 21:34:44', '2022-11-23 21:34:44');
+
+INSERT INTO `arpanet`.`users` (`name`, `email`, `password`, `tipo`, `created_at`, `updated_at`)
+VALUES
+('Daniel', 'daniel@outlook.com', '$2y$10$IFRVH0.rUGzyKFPTX/z7kuSxbm4bq1eSQXh3GS20CEO7DN09oUVDm', 'admin', '2022-11-23 21:34:44', '2022-11-23 21:34:44');
+
+INSERT INTO `arpanet`.`users` (`name`, `email`, `password`, `tipo`, `created_at`, `updated_at`)
+VALUES
+('Gabriel Carlos', 'gabriel@outlook.com', '$2y$10$IFRVH0.rUGzyKFPTX/z7kuSxbm4bq1eSQXh3GS20CEO7DN09oUVDm', 'admin', '2022-11-23 21:34:44', '2022-11-23 21:34:44');
+
+INSERT INTO `arpanet`.`users` (`name`, `email`, `password`, `tipo`, `created_at`, `updated_at`)
+VALUES
+('Diego Albuquerque', 'diego@outlook.com', '$2y$10$IFRVH0.rUGzyKFPTX/z7kuSxbm4bq1eSQXh3GS20CEO7DN09oUVDm', 'admin', '2022-11-23 21:34:44', '2022-11-23 21:34:44');
+
+INSERT INTO `arpanet`.`users` (`name`, `email`, `password`, `tipo`, `created_at`, `updated_at`)
+VALUES
+('Thales', 'thales@outlook.com', '$2y$10$IFRVH0.rUGzyKFPTX/z7kuSxbm4bq1eSQXh3GS20CEO7DN09oUVDm', 'admin', '2022-11-23 21:34:44', '2022-11-23 21:34:44');
