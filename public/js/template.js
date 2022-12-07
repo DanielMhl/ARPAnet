fed78ad8f3f296a7e207a634dcd13826dcdc829e
Daniel Marques <airbokl41@gmail.com>
2022-12-06 23:52:12 -0300

    #ass-cont - crud de contratados e associados / tipo de campos pra number

--- public/js/template.js
+++ public/js/template.js
@@ -16,3 +16,75 @@ function controleTipo() {
         document.getElementById("div-cpfPessoa").required = true;
     }
 };
+
+/* WEBSERVICE */
+function limpaFormularioCep() {
+    //Limpa valores do formulário de cep.
+    document.getElementById('logradouroEndereco').value=("");
+    document.getElementById('bairroEndereco').value=("");
+    document.getElementById('cidadeEndereco').value=("");
+    document.getElementById('ufEndereco').value=("");
+}
+
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        document.getElementById('logradouroEndereco').value=(conteudo.logradouro);
+        document.getElementById('bairroEndereco').value=(conteudo.bairro);
+        document.getElementById('cidadeEndereco').value=(conteudo.localidade);
+        document.getElementById('ufEndereco').value=(conteudo.uf);
+    } //end if.
+    else {
+        //CEP não Encontrado.
+        limpaFormularioCep();
+        var aviso = document.getElementById('div-alert');
+        aviso.style.display = "block";
+        aviso.innerHTML = "CEP não encontrado.";
+    }
+}
+
+function pesquisaCep(valor) {
+
+    //Nova variável "cep" somente com dígitos.
+    var cep = valor.replace(/\D/g, '');
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+
+        //Expressão regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+
+        //Valida o formato do CEP.
+        if(validacep.test(cep)) {
+
+            //Preenche os campos com "..." enquanto consulta webservice.
+            document.getElementById('logradouroEndereco').value="...";
+            document.getElementById('bairroEndereco').value="...";
+            document.getElementById('cidadeEndereco').value="...";
+            document.getElementById('ufEndereco').value="...";
+
+            //Cria um elemento javascript.
+            var script = document.createElement('script');
+
+            //Sincroniza com o callback.
+            script.src = 'https://viacep.com.br/ws/'+ cep + '/json/?callback=meu_callback';
+
+            //Insere script no documento e carrega o conteúdo.
+            document.body.appendChild(script);
+
+        } //end if.
+        else {
+            //cep é inválido.
+            limpaFormularioCep();
+            var aviso = document.getElementById('div-alert');
+            aviso.style.display = "block";
+            aviso.innerHTML = "Formato de CEP inválido.";
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulário.
+        limpaFormularioCep();
+    }
+};
+
+/* endWEBSERVICE */
